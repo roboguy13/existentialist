ec19e7da1b5faa44da538ea13eb1374a378e923e
David Young <roboguy06@gmail.com>
2015-07-16 21:56:16 -0500

    Added constraint based existential stuff

--- src/Data/Existentialist.hs
+++ src/Data/Existentialist.hs
@@ -2,7 +2,8 @@
            , BangPatterns, PatternSynonyms, TypeOperators
            , TypeFamilies, PolyKinds, MultiParamTypeClasses
            , FlexibleInstances, FlexibleContexts, GADTs
-           , PartialTypeSignatures, NamedWildCards
+           , PartialTypeSignatures, NamedWildCards, OverloadedLists
+           , ConstraintKinds
   #-}
 module Data.Existentialist
   (Existential (..)
@@ -17,9 +18,14 @@ module Data.Existentialist
   )
   where
 
-import Control.Monad
+import Control.Monad hiding (Functor, fmap)
+import Prelude hiding (Functor, (.), id, fmap)
+import qualified Prelude
 
-import Data.Foldable
+import Data.Foldable (Foldable)
+import qualified Data.Foldable as F
+
+import GHC.Exts
 
 data Existential f = forall a. Existential (f a)
 
@@ -28,22 +34,37 @@ data AnyList f = forall a. (f a) :| (AnyList f)
                | Nil
 infixr :|
 
-pattern x :> xs = Existential x : xs
+data ConstrList c = forall a. c a => a :> ConstrList c
+                  | CNil
+infixr :>
 
 toExistentials :: AnyList f -> [Existential f]
-toExistentials (x :| xs) = x :> toExistentials xs
+toExistentials (x :| xs) = Existential x : toExistentials xs
 toExistentials Nil       = []
 
 fromExistentials :: Foldable g => g (Existential f) -> AnyList f
-fromExistentials = go . toList
+fromExistentials = go . F.toList
  where
     go :: [Existential f] -> AnyList f
-    go (x :> xs) = x :| go xs
+    go (Existential x : xs) = x :| go xs
     go []        = Nil
+
+{-# RULES "fromExistentials/toExistentials"   fromExistentials . toExistentials = id
+  #-}
 
 anyMap :: (forall a. f a -> b) -> AnyList f -> [b]
 anyMap f (x :| xs) = f x : anyMap f xs
 anyMap _ Nil       = []
+
+constrMap :: (forall a. c a => a -> b) -> ConstrList c -> [b]
+constrMap f (x :> xs) = f x : constrMap f xs
+constrMap f CNil      = []
+
+constrMapM :: Monad m => (forall a. c a => a -> m b) -> ConstrList c -> m [b]
+constrMapM f = sequence . constrMap f
+
+test :: ConstrList Show
+test = 1 :> 'a' :> () :> CNil
 
 anyFoldr :: (forall a. f a -> b -> b) -> AnyList f -> b -> b
 anyFoldr f (x :| xs) z = x `f` anyFoldr f xs z
@@ -62,8 +83,14 @@ anyMapM f xs = sequence (anyMap f xs)
 anyMapM_ :: Monad m => (forall a. f a -> m b) -> AnyList f -> m ()
 anyMapM_ f xs = liftM (const ()) (anyMapM f xs)
 
-
 {-
+test :: IO ()
+test = anyMapM_ printIt $ [Just 'a', Nothing]
+  where
+    printIt (Just _) = putStrLn "just"
+    printIt _        = putStrLn "nothing"
+-}
+
 -- Polykinded Functor instance and associated stuff (Heavily based on the
 -- `hask` package).
 data Nat (p :: i -> i -> *) (q :: j -> j -> *) (f :: i -> j) (g :: i -> j) where
@@ -89,7 +116,7 @@ class (Category (Dom f), Category (Cod f)) => Functor (f :: i -> j) where
   type Cod f :: j -> j -> *
 
   fmap :: Dom f a b -> Cod f (f a) (f b)
-
+{-
 instance Functor (Either a) where
   type Dom (Either a) = (->)
   type Cod (Either a) = (->)
